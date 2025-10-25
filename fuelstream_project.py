209479271bdf0a7293e099665c43996d49c7d1dc
Network71 <bwiniarski2007@gmail.com>
2025-10-25 16:24:59 +0100

    Before GUI

--- fuelstream_project.py
+++ fuelstream_project.py
@@ -2,6 +2,9 @@ import fastf1
 from matplotlib import pyplot as plt
 from fastf1 import plotting
 import numpy as np
+import pandas as pd
+from timple.timedelta import strftimedelta
+from fastf1.core import Laps
 
 def main_menu():
     print("Welcome to fuelstream.")
@@ -9,6 +12,7 @@ def main_menu():
     print("1: Compare fastest laps of two drivers")
     print("2: Position changes throughout a race")
     print("3: View map of track")
+    print("4: Qualifying results")
 
     user_choice = int(input("> "))
 
@@ -18,6 +22,8 @@ def main_menu():
         position_changes()
     elif user_choice == 3:
         track_map()
+    elif user_choice == 4:
+        quali_results()
 
 def drivers_pace():
     print("Format: (Year) (Track) (Session) (Driver1) (Driver2)")
@@ -131,6 +137,53 @@ def track_map():
     plt.xticks([])
     plt.yticks([])
     plt.axis('equal')
+    plt.show()
+
+def quali_results():
+
+    fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme=None)
+
+    #session details
+    print("Format: (Year) (Track)")
+    user_input = input("Enter session details: ")
+    session_details = user_input.split(" ")
+    session_details[0] = int(session_details[0])
+
+    session = fastf1.get_session(session_details[0], session_details[1], 'Q')
+    session.load()
+
+    #driver array
+    drivers = pd.unique(session.laps['Driver'])
+
+    #get fastest lap and create object, sort and reindex
+    list_fastest_laps = list()
+    for drv in drivers:
+        drvs_fastest_lap = session.laps.pick_drivers(drv).pick_drivers(drv).pick_fastest()
+        list_fastest_laps.append(drvs_fastest_lap)
+    fastest_laps = Laps(list_fastest_laps).sort_values(by='LapTime').reset_index(drop=True)
+
+    pole_lap = fastest_laps.pick_fastest()
+    fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
+
+    #team color list
+    team_colors = list()
+    for index, lap in fastest_laps.iterlaps():
+        color = fastf1.plotting.get_team_color(lap['Team'], session=session)
+        team_colors.append(color)
+
+    #plotting
+    fig, ax = plt.subplots()
+    ax.barh(fastest_laps.index, fastest_laps['LapTimeDelta'], color = team_colors, edgecolor='grey')
+    ax.set_yticks(fastest_laps.index)
+    ax.set_yticklabels(fastest_laps['Driver'])
+
+    ax.invert_yaxis()
+
+    ax.set_axisbelow(True)
+    ax.xaxis.grid(True, which='major', linestyle='--', color='black', zorder=-1000)
+
+    lap_time_string = strftimedelta(pole_lap['LapTime'], '%m:%s.%ms')
+    plt.suptitle(f"{session.event['EventName']} {session.event.year} Qualifying\n" f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})")
     plt.show()
 
 main_menu()
