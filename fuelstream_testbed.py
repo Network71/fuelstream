fcb4a31e28bb405ea41fdd83a7a4b6e609473f6e
Network71 <bwiniarski2007@gmail.com>
2025-10-27 14:35:23 +0000

    track_name_input

--- fuelstream_testbed.py
+++ fuelstream_testbed.py
@@ -1,8 +1,3 @@
-from nicegui import ui
 
-ui.label('Hello NiceGUI!')
-
-ui.button('Click me!', on_click=lambda: ui.notify('You clicked me!'))
-
-ui.run(port=5000)
-
+def functionTest():
+    print("Testing button")
